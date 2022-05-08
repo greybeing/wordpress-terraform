71efc5346d5af3c7f6f762a8503b0e4c526c822a
greybeing <adulojugbenga@yahoo.com>
2022-05-08 20:27:22 +0100

    new main.tf

---
+++ .terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version = "4.13.0"
+  hashes = [
+    "h1:wJ9zY+k27og9sc7A9VWVYUIGJQkXiWOd5B0UuQF8mOk=",
+    "zh:215226bc0372077d2ae6dba4e2f08f6361f8e4953d20bc4c682d40fdf5002544",
+    "zh:42777cbdc046181986c0260ea17027ef1364c31d73a57eb0ab539f6e1a3e0780",
+    "zh:78079d2f5fc35f3c43eb2a131cb49c2c77ddd04943bca97080f33355808d39cc",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9c0404a044eae741f10f3d217dc28658e0f04082963918913b024d3305c11e79",
+    "zh:a1b5a53f60d4f7bff1cc84180fef6205c95b8793741dbc8c0564a6200424ca73",
+    "zh:ba6711064a855ddb55924342b70667e9bed660bde8552dc0bde4b7f8947a2ec4",
+    "zh:d0f77ed514d54f7380d7e1ef585d853f50f1bee381d6abbf3a68429b68de6045",
+    "zh:d5c454d2ac9aed01ae00c477192c93d54c8362357a87684a3171055dcec25f44",
+    "zh:dfd381ed7da945cb85b99df843ee7eab339dd1799fa70d1ad3e94331605aad01",
+    "zh:eb6dc84414714f61b9de0ac190c69f598af9b16d144a44f573df484c06c8d4ef",
+    "zh:f02e79599af3f8f63e4b885c5715be3a4060cbf98eb4bf46d616aa0d9f2b5cd3",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/cloudinit" {
+  version = "2.2.0"
+  hashes = [
+    "h1:tQLNREqesrdCQ/bIJnl0+yUK+XfdWzAG0wo4lp10LvM=",
+    "zh:76825122171f9ea2287fd27e23e80a7eb482f6491a4f41a096d77b666896ee96",
+    "zh:795a36dee548e30ca9c9d474af9ad6d29290e0a9816154ad38d55381cd0ab12d",
+    "zh:9200f02cb917fb99e44b40a68936fd60d338e4d30a718b7e2e48024a795a61b9",
+    "zh:a33cf255dc670c20678063aa84218e2c1b7a67d557f480d8ec0f68bc428ed472",
+    "zh:ba3c1b2cd0879286c1f531862c027ec04783ece81de67c9a3b97076f1ce7f58f",
+    "zh:bd575456394428a1a02191d2e46af0c00e41fd4f28cfe117d57b6aeb5154a0fb",
+    "zh:c68dd1db83d8437c36c92dc3fc11d71ced9def3483dd28c45f8640cfcd59de9a",
+    "zh:cbfe34a90852ed03cc074601527bb580a648127255c08589bc3ef4bf4f2e7e0c",
+    "zh:d6ffd7398c6d1f359b96f5b757e77b99b339fbb91df1b96ac974fe71bc87695c",
+    "zh:d9c15285f847d7a52df59e044184fb3ba1b7679fd0386291ed183782683d9517",
+    "zh:f7dd02f6d36844da23c9a27bb084503812c29c1aec4aba97237fec16860fdc8c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = "us-east-2"
+  profile = "default"
+}
+
+resource "aws_security_group" "WPSG" {
+  
+  name = "wpsgtf"
+  ingress{
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "allow ssh"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"  
+  }
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "allow http"
+    from_port = 0
+    to_port = 80
+    protocol = "tcp"
+  } 
+  egress{
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "permit all"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  } 
+  
+}
+
+resource "aws_instance" "wordpressfrontend" {
+  ami           = "ami-0fa49cc9dc8d62c84"
+  instance_type = "t2.medium"
+  key_name = "WPTFkey"
+  security_groups = [ "wpsgtf" ]
+  tags = {
+    app = "wordpress"
+    role = "frontend"
+   user_data_base64 = "${data.cloudinit_config.wordpress.rendered}"
+  }
+}
+resource "aws_eip" "wpip" {
+  instance = aws_instance.wordpressfrontend.id
+  vpc      = true
+  tags = {
+    app = "wordpress"
+  }
+}
+
+
+variable "dbpassword" {
+  type = string
+  default = "terraform"
+}
+
+resource "aws_db_instance" "wordpressbackend" {
+  
+  instance_class = "db.t2.medium"
+  engine = "mysql"
+  publicly_accessible = false
+  allocated_storage = 20
+  db_name = "wordpress"
+  username = "admin"
+  password = var.dbpassword
+  skip_final_snapshot  = true
+  tags = {
+    app = "mysql"
+  }  
+}
+
+data "cloudinit_config" "wordpress" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = <<EOF
+                    #!/bin/bash
+                    sudo yum install httpd -y
+                    sudo systemctl start httpd
+                    sudo systemctl enable httpd
+                    sudo wget https://wordpress.org/wordpress-5.7.2.tar.gz
+                    sudo tar -xzf wordpress-5.7.2.tar.gz
+                    sudo cp -r wordpress/* /var/www/html/
+                    EOF
+    }
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = <<EOF
+                    #!/bin/bash
+                    sudo yum install -y amazon-linux-extras
+                    sudo amazon-linux-extras enable php7.2
+                    sudo yum clean metadata -y
+                    sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd -y
+                    sudo systemctl restart httpd
+                     EOF
+  }
+}
+
+output "WebServerIP" {
+
+    value = aws_instance.wordpressfrontend.public_ip
+    description = "Web Server IP Address"
+}
+output "DatabaseName" {
+
+    value = aws_db_instance.wordpressbackend.name
+    description = "The Database Name!"
+}
+output "DatabaseUserName" {
+
+    value = aws_db_instance.wordpressbackend.username
+    description = "The Database Name!"
+}
+output "DBConnectionString" {
+
+    value = aws_db_instance.wordpressbackend.endpoint
+    description = "The Database connection String!"
+}
